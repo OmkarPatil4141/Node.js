395a1dc190171047ad6ba3a345e0ec00d1433dd4
OmkarPatil4141 <patilomkarr4141@gmail.com>
2024-06-09 17:36:19 +0530

    Study creating own middleware in node.js

--- 4-Natuors/app.js
+++ 4-Natuors/app.js
@@ -5,7 +5,12 @@ const app = express();
 //Middleware
 app.use(express.json())
 
-const port = 3000;
+// Custom Middleware  it is going to run for all requestss
+app.use((req,res,next)=>{
+    req.requestTime = new Date().toISOString();
+    next();
+})
+
 
 /*
 //for routing
@@ -26,8 +31,8 @@ app.post('/',(req, res)=>{
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-//get
-app.get('/api/v1/tours',(req,res)=>{
+const getTours = (req,res)=>{
+    console.log(req.requestTime);
     res.status(200).json({
         "status":"success",
         result : tours.length,
@@ -35,32 +40,29 @@ app.get('/api/v1/tours',(req,res)=>{
             tours
         }
     })
-})
+}
 
-//get by id
-
-app.get('/api/v1/tours/:id',(req,res)=>{
+const getTour = (req,res)=>{
     
-     const id = req.params['id'] * 1; //as everything coming from object is string just typecasted
-    // const id = req.params.id * 1;
-    const tour = tours.find(ele => ele.id === id)
-    if(!tour)
-    {
-        return res.status(400).json({            //return because only one response in order to invalid id
-            status :"Fail",
-            message : "Invalid Id"
-        })
-    }
-    res.status(200).json({
-        status :"suceess",
-        data : {
-            tour
-        }
-    })
-})
+    const id = req.params['id'] * 1; //as everything coming from object is string just typecasted
+   // const id = req.params.id * 1;
+   const tour = tours.find(ele => ele.id === id)
+   if(!tour)
+   {
+       return res.status(400).json({            //return because only one response in order to invalid id
+           status :"Fail",
+           message : "Invalid Id"
+       })
+   }
+   res.status(200).json({
+       status :"suceess",
+       data : {
+           tour
+       }
+   })
+}
 
-//update
-app.delete('/api/v1/tours/:id',(req,res)=>{
+const delteTour = (req,res)=>{
 
     if(req.params.id * 1 > tours.length)
     {
@@ -74,10 +76,9 @@ app.delete('/api/v1/tours/:id',(req,res)=>{
         status : "Success",
         data : null
     })
-})
+}
 
-//delete
-app.patch('/api/v1/tours/:id',(req,res)=>{
+const updateTour = (req,res)=>{
 
     if(req.params.id * 1 > tours.length)
     {
@@ -93,18 +94,14 @@ app.patch('/api/v1/tours/:id',(req,res)=>{
             tour : "updated tour here "
         }
     })
-})
+}
 
 
-
-//create 
-app.post('/api/v1/tours',(req,res)=>{
+const createTour = (req,res)=>{
     // console.log(req.body);
     // res.send('Done')
-
     const NewId = tours[tours.length-1].id + 1;
     const newTour = Object.assign({id : NewId}, req.body)
-
     tours.push(newTour)
 
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=>{
@@ -117,11 +114,43 @@ app.post('/api/v1/tours',(req,res)=>{
                 tours : newTour
             }
         })
-    })
-    
-})
+    })   
+}
+
+
+///////////////////////////////////////////////////////
+// Refactoring code 1
+//////////////////////////////////////////////////////
+/*
+//get
+app.get('/api/v1/tours',getTours);
+//get by id
+app.get('/api/v1/tours/:id',getTour)
+//delete
+app.delete('/api/v1/tours/:id',delteTour)
+//update
+app.patch('/api/v1/tours/:id',updateTour)
+//create 
+app.post('/api/v1/tours',createTour)
+
+*/
+
+
+///////////////////////////////////////////////////////
+// Refactoring code 2
+//////////////////////////////////////////////////////
+
+app.route('/api/v1/tours').get(getTours).post(createTour)
+
+app.route('/api/v1/tours/:id').get(getTour).delete(delteTour).patch(updateTour)
+
+
+
+
+
 
 //we can start server in this way
+const port = 3000;
 app.listen(port,()=>{
     console.log(`Server is started on port ${port}..And waiting for requests`);
 })
